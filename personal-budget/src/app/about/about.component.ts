27960ec45d8ea34f77b9b9372df954a07b717119
Daniel Sorrell <dsorrel3@uncc.edu>
2020-10-12 21:08:11 -0400

    Added Angular

---
+++ personal-budget/src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pb-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
